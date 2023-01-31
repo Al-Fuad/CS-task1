c4936bacbd31963cec6a8f9dacb483e8a1c09745
AlFuad <nur22205341056@diu.edu.bd>
2023-01-31 08:52:51 +0600

    homework3

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        
+        if ( num1 == num2){
+            Console.WriteLine("The numbers are equal.");
+        }
+        else{
+            Console.WriteLine("The numbers are not equal.");
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        
+        if ( num % 2 == 0){
+            Console.WriteLine("The number is even.");
+        }
+        else{
+            Console.WriteLine("The number is odd.");
+        }
+    }
+}
