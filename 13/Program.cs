d4c5fc6c9b33210244baaa221019d8415f80f215
AlFuad <nur22205341056@diu.edu.bd>
2023-02-02 19:11:55 +0600

    13

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter Temperature : ");
+        int temp = Convert.ToInt32(Console.ReadLine());
+        if (temp >= 40){
+            Console.WriteLine("Its Very Hot");
+        }
+        else if (temp < 40 && temp >= 30){
+            Console.WriteLine("Its Hot");
+        }
+        else if (temp < 30 && temp >= 20){
+            Console.WriteLine("Normal in Temp");
+        }
+        else if (temp < 20 && temp >= 10){
+            Console.WriteLine("Cold weather");
+        }
+        else if (temp < 10 && temp >= 0){
+            Console.WriteLine("Very Cold weather");
+        }
+        else if (temp < 0){
+            Console.WriteLine("Freezing weather");
+        }
+    }
+}
